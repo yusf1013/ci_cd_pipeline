3ccbb116e677a6617ce084f3ecae734c3b025d42
Yusuf <bsse1013@iit.du.ac.bd>
2021-12-07 21:17:03 +0600

    added subtract divide multiply

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return {
@@ -9,14 +10,37 @@ def index():
         'app_version': '4'
     }
 
+
 @app.route('/add/<a>/<b>')
 def add_route(a, b):
     return {
         'sum': add(a, b)
     }
 
+
+@app.route('/subtract/<a>/<b>')
+def add_route2(a, b):
+    return {
+        'difference': subtract(a, b)
+    }
+
+
+@app.route('/multiply/<a>/<b>')
+def add_route3(a, b):
+    return {
+        'product': multiply(a, b)
+    }
+
+
+@app.route('/divide/<a>/<b>')
+def add_route4(a, b):
+    return {
+        'ratio': divide(a, b)
+    }
+
 # Input can be both strings and digits
-def add(a,b):
+
+def add(a, b):
     if type(a) == str and not a.isdigit():
         return "Invalid Input"
 
@@ -28,13 +52,45 @@ def add(a,b):
     return a + b
 
 
+def subtract(a, b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+    return a - b
+
+
+def multiply(a, b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+    return a * b
+
+
+def divide(a, b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+
+    if b == 0:
+        return "Division by zero"
+
+    return a / b
+
+
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
-
--- test_index.py
+++ test_index.py
@@ -1,7 +1,17 @@
-from index import add
+from index import add, subtract, multiply, divide
 
 def test_add():
     assert add(1,2) == 3
     assert add(1, "asdf") == "Invalid Input"
+
+    assert subtract(1, 2) == -1
+    assert subtract(1, "asdf") == "Invalid Input"
+
+    assert multiply(1, 2) == 2
+    assert multiply(1, "asdf") == "Invalid Input"
+
+    assert divide(1, 2) == -1
+    assert divide(1, "asdf") == "Invalid Input"
+    assert divide(1, 0) == "Division by zero"
 
     # assert add(1,"abc") == "Invalid Input"
